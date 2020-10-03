9cfa0391fdd5082e57f122be113cec71126bb3f8
Andrew Pungprakearti <andrew@alephsf.com>
2020-10-03 11:10:37 -0700

    Add basic typing test items and create shuffle

--- src/components/TypingTest/TypingTest.tsx
+++ src/components/TypingTest/TypingTest.tsx
@@ -2,9 +2,139 @@ import React from 'react'
 import styles from './TypingTest.module.scss'
 
 const TypingTest = () => {
+  const defText = [
+    'the',
+    'at',
+    'there',
+    'some',
+    'my',
+    'of',
+    'be',
+    'use',
+    'her',
+    'than',
+    'and',
+    'this',
+    'an',
+    'would',
+    'first',
+    'a',
+    'have',
+    'each',
+    'make',
+    'water',
+    'to',
+    'from',
+    'which',
+    'like',
+    'been',
+    'in',
+    'or',
+    'she',
+    'him',
+    'call',
+    'is',
+    'one',
+    'do',
+    'into',
+    'who',
+    'you',
+    'had',
+    'how',
+    'time',
+    'oil',
+    'that',
+    'by',
+    'their',
+    'has',
+    'its',
+    'it',
+    'word',
+    'if',
+    'look',
+    'now',
+    'he',
+    'but',
+    'will',
+    'two',
+    'find',
+    'was',
+    'not',
+    'up',
+    'more',
+    'long',
+    'for',
+    'what',
+    'other',
+    'write',
+    'down',
+    'on',
+    'all',
+    'about',
+    'go',
+    'day',
+    'are',
+    'were',
+    'out',
+    'see',
+    'did',
+    'as',
+    'we',
+    'many',
+    'number',
+    'get',
+    'with',
+    'when',
+    'then',
+    'no',
+    'come',
+    'his',
+    'your',
+    'them',
+    'way',
+    'made',
+    'they',
+    'can',
+    'these',
+    'could',
+    'may',
+    'I',
+    'said',
+    'so',
+    'people',
+    'part',
+  ]
+
+  // Shuffle array
+  const shuffleText = (text: Array<string>) => {
+    let curLen = defText.length - 1
+    let shuffledText = []
+
+    while (curLen > 0) {
+      shuffledText.push(defText[Math.floor(Math.random() * curLen)])
+      curLen -= 1;
+    }
+
+  return shuffledText
+  }
+
+  const words = shuffleText(defText)
+
   return (
     <div className={styles.Wrap}>
-      TypingTest
+      <div className='container'>
+        {words.map((word, i) => {
+          return (
+            <div
+              className={styles.word}
+              key={i}
+            >
+              {word}
+            </div>
+          )
+        })}
+        <input />
+      </div>
     </div>
   )
 }
